e6fda4a5afefa4adc8928980ea425e13bbec48c8
RuturajKotwal <59957303+RuturajKotwal@users.noreply.github.com>
2020-02-24 11:31:49 +0530

    Enemy BOT added
    An Enemy BOT, grey material added. It will attack when player enters his "Look Radius"
    Timestamp - 24 Feb 2020 11:31 am

---
+++ New Unity Project (1)/Assets/EnemyController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    public float lookRadius = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+}
